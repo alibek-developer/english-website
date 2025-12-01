435779d9009ed7f10de11747387119c15105d3bc
AlibekAllaberganov123 <inoqdost478@gmail.com>
2025-12-01 21:34:25 +0500

    feat: fix commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,17 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
-	base: '/',
+	// base: '/',  // ❗ Deploy uchun emas bo‘lsa, olib tashlagan yaxshi
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname),
+			'@': path.resolve(__dirname, './src'),
 			'~backend/client': path.resolve(__dirname, './client'),
 			'~backend': path.resolve(__dirname, '../backend'),
 		},
