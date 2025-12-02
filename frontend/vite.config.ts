d01d2fb8731fd68d5eff0d45171d34408b474a5a
AlibekAllaberganov123 <inoqdost478@gmail.com>
2025-12-02 14:28:16 +0500

    featttt: hjsvakjcns

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
 const __filename = fileURLToPath(import.meta.url)
@@ -10,9 +10,23 @@ const __dirname = path.dirname(__filename)
 export default defineConfig({
 	plugins: [tailwindcss(), react()],
 	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-			'~backend': path.resolve(__dirname, '../backend'),
+		alias: [
+			{ find: '@', replacement: path.resolve(__dirname, 'src') },
+			{
+				find: '~backend/client',
+				replacement: path.resolve(__dirname, 'src/client.ts'),
+			},
+			{ find: '~backend', replacement: path.resolve(__dirname, '../backend') },
+		],
+		extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: 'assets/[name]-[hash].js',
+				chunkFileNames: 'assets/[name]-[hash].js',
+				assetFileNames: 'assets/[name]-[hash][extname]',
+			},
 		},
 	},
 })
